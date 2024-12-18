bfb56977280abffc85da8b1be4c166f2ecaf1c16
Jean <xmodgamecenterhon@gmail.com>
2024-12-18 07:54:00 +0000

    adding dockerCompose file and working with 4 container

--- FRONT/src/App.js
+++ FRONT/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/tasks"; // Ваш URL бэкенда
+const API_URL = "/api/tasks"; // Ваш URL бэкенда
 
 // Получение задач с сервера
 const fetchTasks = async () => {
