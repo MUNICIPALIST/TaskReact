75386188bc368d5829873bf86c70198ab04b632e
Jean <xmodgamecenterhon@gmail.com>
2025-01-30 12:57:37 +0000

    last update on server

--- BACK/task-back/src/main/java/com/example/task_back/controller/TaskController.java
+++ BACK/task-back/src/main/java/com/example/task_back/controller/TaskController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3001", "https://bananadev.me"})
 public class TaskController {
 
     private final TaskService taskService;
